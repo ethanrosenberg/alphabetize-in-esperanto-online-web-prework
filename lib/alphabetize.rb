c373bb97466efae40b2074e6c359bf6e7bfc478a
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-14 06:41:06 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,6 +3,7 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
   arr.sort_by do |sent|
+    
     temp = sent.split("")
     number_index = temp.map { |item| ESPERANTO_ALPHABET.index(item) }
     end
